fa4780b61b4b9c83a36c4c67354f8892a4e4aded
YangDaijun <yangdaijun@migu.cn>
2023-05-25 18:34:38 +0800

    test2

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -2,45 +2,50 @@ const webpack = require('atool-build/lib/webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var conf = {
-  filename: 'index.html',
-  template: './demo/index.html',
-  inject: true,
-  minify: {
-    removeComments: true,
-    collapseWhitespace: false
-  },
-  hash: true,
+    filename: 'index.html',
+    template: './demo/index.html',
+    inject: true,
+    minify: {
+        removeComments: true,
+        collapseWhitespace: false
+    },
+    hash: true,
 }
 
+/**
+ * test2
+ * @param webpackConfig
+ * @returns {*}
+ */
 module.exports = function (webpackConfig) {
-  webpackConfig.babel.plugins.push(['import', {
-    libraryName: 'antd',
-    style: true,
-  }]);  
-  webpackConfig.entry = {
-    index: './demo/index.tsx',
-  };
+    webpackConfig.babel.plugins.push(['import', {
+        libraryName: 'antd',
+        style: true,
+    }]);
+    webpackConfig.entry = {
+        index: './demo/index.tsx',
+    };
 
-  webpackConfig.output.path = './docs';
-  webpackConfig.output.publicPath = '/react-viewer/';
+    webpackConfig.output.path = './docs';
+    webpackConfig.output.publicPath = '/react-viewer/';
 
-  webpackConfig.module.loaders.forEach(function (loader, index) {
-    if (loader.test.toString().indexOf('html') > 0) {
-      loader.loader = 'html';
-    }
-  });
+    webpackConfig.module.loaders.forEach(function (loader, index) {
+        if (loader.test.toString().indexOf('html') > 0) {
+            loader.loader = 'html';
+        }
+    });
 
-  webpackConfig.plugins.push(
-    new HtmlWebpackPlugin(conf)
-  );
+    webpackConfig.plugins.push(
+        new HtmlWebpackPlugin(conf)
+    );
 
-  webpackConfig.plugins.some(function (plugin, i) {
-    if (plugin instanceof webpack.optimize.CommonsChunkPlugin) {
-      webpackConfig.plugins.splice(i, 1);
+    webpackConfig.plugins.some(function (plugin, i) {
+        if (plugin instanceof webpack.optimize.CommonsChunkPlugin) {
+            webpackConfig.plugins.splice(i, 1);
 
-      return true;
-    }
-  });
+            return true;
+        }
+    });
 
-  return webpackConfig;
+    return webpackConfig;
 };
